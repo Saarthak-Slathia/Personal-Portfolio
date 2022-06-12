69645a7a700d582bf84f1df2ba99ac04e449e69d
Saarthak-Slathia <saarthak.slathia@gmail.com>
2022-06-12 14:04:36 +0530

    Initial Commit

---
+++ Js/script.js
@@ -0,0 +1,47 @@
+const loginBtn = document.getElementById("login");
+const signupBtn = document.getElementById("signup");
+const loginFooter = document.getElementById("loginbtnFooter");
+const submit = document.getElementById("submit");
+const moreBtn = document.getElementById("moreBtn");
+
+loginBtn.onclick = () => {
+  swal(
+    "Features under Development",
+    "These features are currently under development",
+    "info"
+  );
+};
+
+signupBtn.onclick = () => {
+  swal(
+    "Features under Development",
+    "These features are currently under development",
+    "info"
+  );
+};
+
+loginFooter.onclick = () => {
+  swal(
+    "Features under Development",
+    "These features are currently under development",
+    "info"
+  );
+};
+
+submit.onclick = e => {
+  e.preventDefault();
+  swal(
+    "Features under Development",
+    "These features are currently under development",
+    "info"
+  );
+};
+
+moreBtn.onclick = e => {
+  e.preventDefault();
+  swal(
+    "Features under Development",
+    "These features are currently under development",
+    "info"
+  );
+};
